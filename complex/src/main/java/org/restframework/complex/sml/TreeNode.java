f5c4cdfa2727e888d26beffc8fae0dc07ca887ad
JVP11292021 <jessyvanpolanen@gmail.com>
2024-01-20 20:21:16 +0100

    Update TreeNode.java

--- complex/src/main/java/org/restframework/complex/sml/TreeNode.java
+++ complex/src/main/java/org/restframework/complex/sml/TreeNode.java
@@ -1,5 +1,6 @@
 package org.restframework.complex.sml;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.function.Function;
@@ -13,10 +14,31 @@ import java.util.function.Function;
  * @version 1.0
  */
 @Data
+@AllArgsConstructor
 public class TreeNode {
+    /**
+     * The question associated with an internal node.
+     * If null, the node is a leaf node.
+     */
     public Function<Double[], Boolean> question;
+
+    /**
+     * Flag indicating whether the node is a leaf node.
+     */
     private boolean isLeaf;
+
+    /**
+     * The left subtree of the node. Null for leaf nodes.
+     */
     private TreeNode left;
+
+    /**
+     * The right subtree of the node. Null for leaf nodes.
+     */
     private TreeNode right;
+
+    /**
+     * The prediction value for leaf nodes. Null for internal nodes.
+     */
     private Double prediction;
 }
